b7ab6868df563dfe20178e401e36653d2d799684
Prakash reddy <prakashreddy.imduri@criticalriver.com>
2020-09-18 16:52:08 +0530

    Removing theme

--- docs/conf.py
+++ docs/conf.py
@@ -69,13 +69,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_theme_path = ['_themes']
+#html_theme_path = ['_themes']
 # Setup the breathe extension
 # breathe_projects = {
 #      "CypressOverTheAir": "../docs/api_reference_manual/xml"
