bf366752a22abbc1b10e86d78b87212053090ea6
Prakash reddy <prakashreddy.imduri@criticalriver.com>
2020-09-18 14:48:48 +0530

    theme added

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_theme_path = ['_themes']
 # Setup the breathe extension
 # breathe_projects = {
 #      "CypressOverTheAir": "../docs/api_reference_manual/xml"
